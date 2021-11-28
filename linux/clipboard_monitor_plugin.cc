4aa99c8ad79e7487258c09bf004e6a2c00afff61
Tim Uy <tim@gig8.com>
2021-11-28 08:12:33 -0800

    add placeholders for monitorClipboard

--- linux/clipboard_monitor_plugin.cc
+++ linux/clipboard_monitor_plugin.cc
@@ -30,6 +30,10 @@ static void clipboard_monitor_plugin_handle_method_call(
     g_autofree gchar *version = g_strdup_printf("Linux %s", uname_data.version);
     g_autoptr(FlValue) result = fl_value_new_string(version);
     response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+  } else if (strcmp(method, "monitorClipboard") == 0) {
+    printf("monitorClipboard\n");
+  } else if (strcmp(method, "stopMonitoringClipboard") == 0) {
+    printf("stopMonitoringClipboard\n");
   } else {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
